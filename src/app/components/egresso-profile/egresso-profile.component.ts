22b0b8f73c5b49277de788747e79f145995fc77d
Railton <railtonplayrx@gmail.com>
2025-03-26 20:25:04 -0300

    feat: Egresso Profile Component

--- src/app/components/egresso-profile/egresso-profile.component.ts
+++ src/app/components/egresso-profile/egresso-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EgressoService } from '../../services/egresso.service';
 
 @Component({
   selector: 'app-egresso-profile',
@@ -7,5 +9,62 @@ import { Component } from '@angular/core';
   styleUrl: './egresso-profile.component.scss'
 })
 export class EgressoProfileComponent {
+
+  constructor(private route: ActivatedRoute, private egressoService: EgressoService){}
+
+  egresso: any = {} as any;
+  cursos: any[] = [];
+  cargos: any[] = [];
+
+  ngOnInit(){
+
+    this.route.paramMap.subscribe(params => {
+
+      const cpf = params.get('cpf');
+
+      if(cpf){
+
+        this.egressoService.recuperarEgresso(cpf).subscribe(
+          {
+            next: (response) => {
+              
+             console.log(response)
+
+             this.egresso = response
+
+             this.egressoService.recuperarCurso(response.id).subscribe(
+              {
+                next: (response) => {
+                  this.cursos = response.content
+
+                  console.log(response.content)
+
+
+                }
+              }
+            )
+
+            this.egressoService.recuperarCargos(response.id).subscribe(
+              {
+                next: (response) => {
+                  this.cargos = response.content
+
+                  console.log(response.content)
+
+                }
+              }
+            )
+
+
+            }
+          }
+        );
+        
+      }
+    });
+
+  
+
+  }
 
 }
