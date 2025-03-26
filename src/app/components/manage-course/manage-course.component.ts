ad0a9f3de2150467abe19185a91fb5362feb13d3
Railton <railtonplayrx@gmail.com>
2025-03-26 20:18:17 -0300

    refactor: manage-egresso component

--- src/app/components/manage-course/manage-course.component.ts
+++ src/app/components/manage-course/manage-course.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ICourse } from '../../interfaces/course.interface';
-import { ManageCourseService } from '../../services/manage-course.service';
 import { CourseService } from '../../services/course.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-course',
@@ -10,89 +10,17 @@ import { CourseService } from '../../services/course.service';
   styleUrl: './manage-course.component.scss'
 })
 export class ManageCourseComponent {
-  constructor(private manageCourseService: ManageCourseService, private courseService: CourseService) {}
+  constructor(private courseService: CourseService, private router: Router) {}
 
 
   courseFiltrados: any[] = [];
   searchTerm: string = '';
   paginaAtual: number = 1;
   itensPorPagina: number = 6;
-  totalPaginas: number = 4;
+  totalPaginas: number = 0;
   coursePaginados: any[] = [];
 
-  course: ICourse[]  =  [
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-    
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-      {
-        "id": 1,
-        "name": "Curso Curso 1",
-        "level": "1",
-        "createdAt": "2025-03-11T22:21:23.741944",
-        "updatedAt": "2025-03-11T22:21:23.741944",
-      },
-  ]
+  courses: any[] = []
 
   // modal 
 
@@ -101,32 +29,67 @@ export class ManageCourseComponent {
   viewCourse: any = {} as any;
 
   ngOnInit(): void {
-    this.filtrarCourse();
+
+    this.getCursos();
+
   }
 
-  filtrarCourse() {
-    if (this.searchTerm.trim() === '') {
-      this.courseFiltrados = this.course;
-    } else {
-      const termo = this.searchTerm.toLowerCase();
-      this.courseFiltrados = this.course.filter((e) =>
-        e.name.toLowerCase().includes(termo)
-      );
+  getCursos(){
+
+    this.courseService.getAllCourses(this.paginaAtual -1, this.itensPorPagina).subscribe(
+      {
+        next: (response) => {
+
+          this.courses = response.content;
+
+          this.totalPaginas = response.totalPages;
+        
+          console.log(response)
+
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => {
+
+        }
+      }
+    )
+
+  }
+
+  
+  verificarCampo(event: Event) {
+    const valor = (event.target as HTMLInputElement).value;
+    if (valor.trim() === '') {
+      this.buscarCurso();
+    }
+  }
+  
+  buscarCurso(){
+
+    console.log(this.searchTerm);
+
+    if(this.searchTerm == ''){
+      this.getCursos();
+      return
     }
 
-    this.paginaAtual = 1;
-    this.atualizarPaginacao();
+    this.paginaAtual = 1
+    
+    this.courseService.getCourseByName(this.paginaAtual - 1, this.totalPaginas, this.searchTerm).subscribe(
+      {
+        next: (response) => {
+          this.courses = response.content
+        }
+      }
+    )
   }
 
   atualizarPaginacao() {
-    this.totalPaginas = Math.ceil(
-      this.courseFiltrados.length / this.itensPorPagina
-    );
+    
+    this.getCursos();
 
-    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
-    const fim = inicio + this.itensPorPagina;
-
-    this.coursePaginados = this.courseFiltrados.slice(inicio, fim);
   }
 
   paginaAnterior() {
@@ -145,12 +108,13 @@ export class ManageCourseComponent {
 
   criarNovoCourse() {
 
-    this.manageCourseService.emitirCourse({operation: true})
+    this.router.navigate(['/cadastrar_curso']);
+
   }
 
   editarCourse(course: ICourse) {
 
-    this.manageCourseService.emitirCourse({operation: false, course: course})
+    this.router.navigate(['/atualizar_curso', course.id]);
 
   }
 
@@ -168,12 +132,10 @@ export class ManageCourseComponent {
 
   excluirCourse(course: ICourse) {
   
-    alert('excluir');
-
     this.courseService.deleteCourse(course.id).subscribe(
       {
         next: (response) => {
-
+          this.buscarCurso()
         },
         error: (error) => {
           console.error(error);
