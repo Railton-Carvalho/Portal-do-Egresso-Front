518529175b29010ab81787d7a9f2c519b47c4956
Railton <railtonplayrx@gmail.com>
2025-03-26 20:22:28 -0300

    feat: registers components module

--- src/app/components/registers/create-course/create-course.component.ts
+++ src/app/components/registers/create-course/create-course.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../../../services/course.service';
+import { ICourse } from '../../../interfaces/course.interface';
 
 @Component({
   selector: 'app-create-course',
@@ -14,11 +17,106 @@ export class CreateCourseComponent {
   
   title: 'Cadastrar' | 'Atualizar' = 'Cadastrar';
 
+  oportunidades: any[] = [];
+  cursoId: number = -1;
+
   levelsList: string[]=[
-    'Bacharelado',
+    "Bacharelado",
     'Mestrado',
     'Doutourado',
     'Especialista'
   ];
+
+  constructor(private route: ActivatedRoute, private courseService: CourseService) {}
+
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(params => {
+
+      const id = Number(params.get('id')); 
+
+      if(id){
+        this.title = 'Atualizar';
+
+      
+        this.courseService.getCourseById(id).subscribe(
+          {
+            next: (response) => {
+
+              console.log(response)
+              
+              //Preenche o formulário com os dados
+              this.nameFormControl.setValue(response.nome);
+              this.levelFormControl.setValue(response.nivel);
+
+              this.cursoId = response.id;
+              this.oportunidades = response.oportunidades;
+
+            }
+          }
+        );
+        
+      }
+
+    });
+
+  }
+
+  onSubmit(){
+
+    if(this.title === 'Cadastrar'){
+
+      let curso = {
+        nome: this.nameFormControl.value ?? '',
+        nivel: this.levelFormControl.value ?? '',
+        oportunidades: null,
+        ano_inicio: new Date().getFullYear(),
+        ano_fim: new Date().getFullYear(),
+      }
+      
+      this.courseService.createCourse(curso).subscribe(
+        {
+          next: (response) =>{
+            console.log(response)
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {
+
+          }
+        }
+      )
+
+    }else{
+
+      let curso = {
+        id: this.cursoId,
+        nome: this.nameFormControl.value ?? '',
+        nivel: this.levelFormControl.value ?? '',
+        oportunidades: this.oportunidades,
+        ano_inicio: new Date().getFullYear(),
+        ano_fim: new Date().getFullYear(),
+      }
+
+      this.courseService.updateCourse(curso).subscribe(
+        {
+          next: (response) =>{
+            
+            console.log(response)
+
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => {
+            
+          }
+        }
+      )
+
+    }
+
+  }
 
 }
--- src/app/components/registers/register-egresso/register-egresso.component.ts
+++ src/app/components/registers/register-egresso/register-egresso.component.ts
@@ -10,10 +10,10 @@ import {
 import {ErrorStateMatcher} from '@angular/material/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { ManageEgressoService } from '../../../services/manage-egresso.service';
-import { IEgresso } from './../../../interfaces/egresso.interface';
-import { RegisterEgressoService } from '../../../services/register-egresso.service';
+import { IEgresso, IEgressoCreate } from './../../../interfaces/egresso.interface';
 import { EgressoService } from '../../../services/egresso.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CourseService } from '../../../services/course.service';
 
 @Component({
   selector: 'app-register-egresso',
@@ -24,7 +24,7 @@ import { EgressoService } from '../../../services/egresso.service';
 export class RegisterEgressoComponent {
 
 
-  constructor(private registerEgressoService: RegisterEgressoService, private egressoService: EgressoService ){}
+  constructor(private egressoService: EgressoService, private route: ActivatedRoute, private router: Router, private courseService: CourseService){}
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
@@ -34,57 +34,195 @@ export class RegisterEgressoComponent {
 
   fullNameFormControl =  new FormControl('', [Validators.required]);
 
-  passwordFormControl =  new FormControl('', [Validators.required]);
+  passwordFormControl =  new FormControl(null, [Validators.required]);
+
+  cursoFormControl = new FormControl('', [Validators.required]);
+
+  dataInicio = new FormControl('', [Validators.required]);
+  dataFim = new FormControl('', [Validators.required]);
 
 
   matcher = new MyErrorStateMatcher();
   
-  courseList: string[]=[
-    'Ciência da Computação',
-    'Logistica',
-    'Sistemas de Informação',
-    'Engenharia de Software'
-  ];
+  courseList: any[]= []
 
   title: 'Cadastrar' | 'Atualizar' = 'Cadastrar';
+
+  egressoId: number = -1;
+  egressoStatus: string = '';
+  egressoHomologado: string = '';
   
   ngOnInit(): void {
-    const dados = this.registerEgressoService.getLastEgresso();
 
-    if (dados && dados.operation == false) {
-      const egresso = dados.egresso;
+    this.route.paramMap.subscribe(params => {
+      
+      const cpf = params.get('cpf'); 
 
-      this.title = 'Atualizar'
+      const id = Number(params.get('id'));
 
-      this.emailFormControl.setValue(egresso.email);
-      this. 
-        cpfFormControl 
-      .setValue(egresso.cpf);
-      this.resumoFromControl.setValue(egresso.resumo)
-      this.fullNameFormControl.setValue(egresso.nome);
-      this.passwordFormControl.setValue(egresso.senha);
-    }
+      if(cpf){
+
+        this.title = 'Atualizar';
+
+
+        this.egressoService.recuperarEgresso(cpf).subscribe(
+          {
+            next: (response) => {
+              
+              console.log(response)
+
+              //Preenche o formulário com os dados
+              this.egressoId = response.id;
+              this.egressoStatus = response.userStatus;
+              this.egressoHomologado = response.homologadoStatus;
+              // Preenche o formulário com os dados
+              this.emailFormControl.setValue(response.email);
+              this.cpfFormControl.setValue(response.cpf);
+              this.resumoFromControl.setValue(response.resumo);
+              this.fullNameFormControl.setValue(response.nome);
+              //this.passwordFormControl.setValue(response.senha);
+              //this.cursoFormControl.setValue(response.egressoCursos);
+
+              this.passwordFormControl.clearValidators();
+
+              this.passwordFormControl.updateValueAndValidity();
+
+              
+            this.egressoService.recuperarCurso(response.id).subscribe(
+              {
+                next: (response) => {
+
+                  console.log(response)
+
+                  this.cursoFormControl.setValue(response.content[0].id)
+                  //this.dataInicio.setValue(response.content[0])
+
+                },complete: () =>{  
+                  
+                }
+              },
+            )
+
+
+            }, error: (error) => {
+
+              console.error(error)
+
+              if (error instanceof SyntaxError) {
+                console.error('Erro de JSON inválido na resposta da API.');
+              }
+            }
+          }
+        );
+
+  
+      }else if(id){
+
+        this.title = 'Atualizar';
+
+
+        this.egressoService.getEgressoById(id).subscribe(
+          {
+            next: (response) => {
+              
+              console.log(response)
+
+              //Preenche o formulário com os dados
+              this.egressoId = response.id;
+              this.egressoStatus = response.userStatus;
+              this.egressoHomologado = response.homologadoStatus;
+              // Preenche o formulário com os dados
+              this.emailFormControl.setValue(response.email);
+              this.cpfFormControl.setValue(response.cpf);
+              this.resumoFromControl.setValue(response.resumo);
+              this.fullNameFormControl.setValue(response.nome);
+              //this.passwordFormControl.setValue(response.senha);
+    
+              this.passwordFormControl.clearValidators();
+
+              this.passwordFormControl.updateValueAndValidity();
+
+                
+            this.egressoService.recuperarCurso(response.id).subscribe(
+              {
+                next: (response) => {
+
+                  console.log(response)
+
+                  this.cursoFormControl.setValue(response.content[0].id);
+                  this.dataInicio.setValue(response.content[0].ano_inicio);
+                  this.dataFim.setValue( response.content[0].ano_fim);
+
+                },complete: () =>{  
+                  
+                }
+              },
+            )
+
+            }, error: (error) => {
+
+              console.error(error)
+
+              if (error instanceof SyntaxError) {
+                console.error('Erro de JSON inválido na resposta da API.');
+              }
+            }
+          }
+        );
+
+      }
+      
+    });
+
+    this.courseService.getAllCourses(0, 100).subscribe(
+      {
+        next: (response) => {
+          this.courseList = response.content
+
+          console.log(this.courseList)
+        }
+      }
+    )
+
+    
   }
+
+
+  
 
   onSubmit(){
 
-    const egresso = {
-      nome: this.fullNameFormControl.value,
-      cpf: this.cpfFormControl.value,
-      email: this.emailFormControl.value,
-      resumo: this.resumoFromControl.value,
-      senha: this.passwordFormControl.value,
-    }
+    console.log(this.passwordFormControl.value)
 
     if(this.title === 'Cadastrar'){
+
+      let egresso: IEgressoCreate = {
+        nome: this.fullNameFormControl.value ?? '',
+        cpf: this.cpfFormControl.value ?? '',
+        email: this.emailFormControl.value ?? '',
+        senha: this.passwordFormControl.value ?? null,
+        resumo: this.resumoFromControl.value ?? '',
+        status: 'ACTIVE',
+        homologado: "PENDING",
+        idCurso: Number(this.cursoFormControl.value),
+        ano_inicio: new Date(this.dataInicio.value ?? '').getFullYear() ?? 0,
+        ano_fim: new Date(this.dataFim.value ?? '').getFullYear() ?? 0,
+        Ano_inicio: new Date(this.dataInicio.value ?? '').getFullYear() ?? 0,
+        Ano_fim: new Date(this.dataFim.value ?? '').getFullYear() ?? 0,
+      }
+
+      console.log(egresso)
+
       
       this.egressoService.cadastrarEgresso(egresso).subscribe(
         {
           next: (response) =>{
+
+            console.log(response)
             
           },
           error: (error) => {
-            alert(error);
+            console.error(error);
           },
           complete: () => {
 
@@ -94,13 +232,32 @@ export class RegisterEgressoComponent {
 
     }else{
 
+      let egresso = {
+        id: this.egressoId,
+        nome: this.fullNameFormControl.value,
+        cpf: this.cpfFormControl.value,
+        email: this.emailFormControl.value,
+        senha: this.passwordFormControl.value ?? null,
+        resumo: this.resumoFromControl.value,
+        status: this.egressoStatus,
+        homologado: this.egressoHomologado,
+        idCurso: Number(this.cursoFormControl.value),
+        ano_inicio: new Date(this.dataInicio.value ?? '').getFullYear() ?? null,
+        ano_fim: new Date(this.dataFim.value ?? '').getFullYear() ?? null,
+        Ano_inicio: new Date(this.dataInicio.value ?? '').getFullYear() ?? null,
+        Ano_fim: new Date(this.dataFim.value ?? '').getFullYear() ?? null,
+      }
+
+      console.log(egresso)
+
       this.egressoService.atualizarEgresso(egresso).subscribe(
+
         {
           next: (response) =>{
-            
+            console.log(response)
           },
           error: (error) => {
-            alert(error);
+            console.error(error);
           },
           complete: () => {
             
